5e3a30d9b32458bd534fbebab830273301f5d513
g.dimitropoulos <g.dimitropoulos@sotec.eu>
2023-04-04 14:26:13 +0200

    pubsub set MessageRetentionDuration to 10 min

--- device-communication/src/main/java/org/eclipse/hono/communication/api/service/communication/PubSubService.java
+++ device-communication/src/main/java/org/eclipse/hono/communication/api/service/communication/PubSubService.java
@@ -40,6 +40,7 @@ import com.google.cloud.pubsub.v1.SubscriptionAdminClient;
 import com.google.cloud.pubsub.v1.SubscriptionAdminSettings;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.protobuf.ByteString;
+import com.google.protobuf.util.Durations;
 import com.google.pubsub.v1.ProjectName;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.PubsubMessage;
@@ -54,6 +55,7 @@ import com.google.pubsub.v1.TopicName;
 public class PubSubService implements InternalMessaging {
 
     public static final String COMMUNICATION_API_SUBSCRIPTION_NAME = "%s-communication-api";
+    private static final long MESSAGE_RETENTION = 600000;
     private final Logger log = LoggerFactory.getLogger(PubSubService.class);
     private final Map<String, Subscriber> activeSubscriptions = new HashMap<>();
 
@@ -158,12 +160,14 @@ public class PubSubService implements InternalMessaging {
 
             if (existing.isEmpty()) {
 
-                subscriptionAdminClient.createSubscription(
-                        subscriptionName.toString(),
-                        topicName,
-                        PushConfig.getDefaultInstance(),
-                        50
-                );
+                final Subscription request = Subscription.newBuilder()
+                        .setName(subscriptionName.toString())
+                        .setTopic(topic)
+                        .setPushConfig(PushConfig.getDefaultInstance())
+                        .setAckDeadlineSeconds(0)
+                        .setMessageRetentionDuration(Durations.fromMillis(MESSAGE_RETENTION))
+                        .build();
+                final Subscription createdSubscription = subscriptionAdminClient.createSubscription(request);
             }
         }
         return subscriptionName;
